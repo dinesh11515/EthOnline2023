de39f3c7155b9245f173eeab87accffe2e06265e
dinesh11515 <dineshaitham2@gmail.com>
2023-10-22 15:09:24 +0530

    added sismoconnect

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    serverComponentsExternalPackages: ["@sismo-core/sismo-connect-server"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

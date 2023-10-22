2100e55f9206e8a08af658d2aaca6fa27dc46479
dinesh11515 <dineshaitham2@gmail.com>
2023-10-22 15:53:17 +0530

    FIXED DEPLOYMENT ERROR

---
+++ frontend/pages/chat/index.js
@@ -0,0 +1,11 @@
+import { ChatViewComponent } from "@pushprotocol/uiweb";
+
+export function Chat() {
+  return (
+    <ChatViewComponent
+      chatId="b8e068e02fe12d7136bc2f24408835573f30c6fbf0b65ea26ab4c7055a2c85f1"
+      limit={10}
+      isConnected={true}
+    />
+  );
+}

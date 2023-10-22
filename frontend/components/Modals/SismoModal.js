9427b0c004e46d411fea1d72bb90598b8e806770
Aman-Mandal <002amankumar@gmail.com>
2023-10-22 16:52:01 +0530

    Minor changes

--- frontend/components/Modals/SismoModal.js
+++ frontend/components/Modals/SismoModal.js
@@ -8,6 +8,7 @@ const SismoModal = () => {
     <>
       <Backdrop />
       <div className='w-[500px] font-Poppins bg-[#0f172b] p-10 z-20 rounded-xl text-white flex flex-col fixed left-[50%] -translate-x-[50%] top-[50%] -translate-y-[50%]'>
+        <p className='font-Poppins text-white mb-6 text-center font-semibold'>Connect to assure us that you're a Degen!</p>
         <Image
           className='mx-auto mb-4'
           src={'/assets/astronaut_hurray.png'}
